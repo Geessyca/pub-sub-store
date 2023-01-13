fdc3c59943b5e976a3b5ad65bbce19d81229d3e3
moreira.gcleonel@gmail.com <gessycadclm@gmail.com>
2023-01-12 22:13:57 -0300

    report

--- services/report/app.js
+++ services/report/app.js
@@ -5,10 +5,10 @@ require('dotenv').config({ path: path.resolve(__dirname, '.env') })
 
 var report = {}
 async function updateReport(products) {
-    for(let product of products) {
-        if(!product.name) {
+    for (let product of products) {
+        if (!product.name) {
             continue
-        } else if(!report[product.name]) {
+        } else if (!report[product.name]) {
             report[product.name] = 1;
         } else {
             report[product.name]++;
@@ -17,14 +17,19 @@ async function updateReport(products) {
 
 }
 
-async function printReport() {
-    for (const [key, value] of Object.entries(report)) {
-        console.log(`${key} = ${value} vendas`);
-      }
+async function processMessage(msg) {
+    const reportData = JSON.parse(msg.content)
+    updateReport(reportData.products)
+    printReport()
 }
 
 async function consume() {
-    //TODO: Constuir a comunicação com a fila 
-} 
+    console.log(
+        `✔ INSCRITO COM SUCESSO NA FILA: ${process.env.RABBITMQ_QUEUE_NAME}`
+    );
+
+    await (await RabbitMQService.getInstance()).consume(process.env.RABBITMQ_QUEUE_NAME, (msg) => { processMessage(msg); });
+
+}
 
 consume()
